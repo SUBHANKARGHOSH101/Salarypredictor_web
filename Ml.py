bc71220ca1001705326ad57250d439e0a7697354
HERO-DEL <ghoshsubhankar1230@gmail.com>
2022-10-10 16:45:09 +0530

    initial

--- Ml.py
+++ Ml.py
@@ -12,13 +12,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model=LinearRegression()
 model.fit(X_train, y_train)
-
+y_pred =model.predict(X_test)
+accuracy = int(r2_score(y_test,y_pred)*100)
 st.title("EXPECTED SALARY PREDICTION")
 experience=st.number_input("Enter Years of experience :")
 
 if st.button("SUBMIT"):
 
     st.markdown("<h1 style='color: green;'><b>Expected Salary :</b></h1>",unsafe_allow_html=True)
-    output=model.predict(np.array(experience).reshape(1,-1))[0]
-    st.write(output)
-    st.markdown("<h1 style='color: green;'><b>Expected Salary :</b></h1>",unsafe_allow_html=True)
+    output=int(model.predict(np.array(experience).reshape(1,-1))[0])
+    st.markdown(f'<h4 style="color: green;">{output}</h4>',unsafe_allow_html=True)
+    st.markdown("<h1 style='color: green;'><b>Accuracy :</b></h1>",unsafe_allow_html=True)
+    st.markdown(f'<h4 style="color: green;">{accuracy}%</h4>',unsafe_allow_html=True)
